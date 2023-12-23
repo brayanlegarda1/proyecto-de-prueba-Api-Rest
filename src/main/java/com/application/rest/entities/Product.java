724b498b09afc33c1298f739d11bab1ea77f5896
Brayan Legarda Villegas <brayanlegarda33@gmail.com>
2023-12-23 10:10:45 -0500

    Creación de capa implementación

---
+++ src/main/java/com/application/rest/entities/Product.java
@@ -0,0 +1,28 @@
+package com.application.rest.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "producto")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nombre")
+    private String name;
+    @Column(name = "precio")
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "id_fabricante", nullable = false)
+    private Maker maker;
+}
